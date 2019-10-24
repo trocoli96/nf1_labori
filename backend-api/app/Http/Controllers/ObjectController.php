246ec4bfaa6b5fc45b7771a64f390aac3ece66eb
Alfonso <alfonso.trocoli@gmail.com>
2019-10-24 21:03:18 +0200

    php

--- backend-api/app/Http/Controllers/ObjectController.php
+++ backend-api/app/Http/Controllers/ObjectController.php
@@ -7,6 +7,29 @@ namespace App\Http\Controllers;
 class ObjectController extends Controller
 {
     public function returnUser($id) {
-        return $id ;
+        $user = array(
+            "id" => $id,
+            "name" => "Juan",
+            "email" => 'juan@test.com'
+        );
+        return $user ;
+    }
+    public function returnUsers() {
+        $user1 = array(
+            "id" => 0,
+            "name" => "Juan",
+            "email" => 'juan@test.com'
+        );
+        $user2 = array(
+            "id" => 1,
+            "name" => "Juan",
+            "email" => 'juan@test.com'
+        );
+        $user3 = array(
+            "id" => 2,
+            "name" => "Juan",
+            "email" => 'juan@test.com'
+        );
+        return [$user1, $user2, $user3] ;
     }
 }
