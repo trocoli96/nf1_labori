62f82f42c424f0a1e8aaf2b13b869ee609d72305
braiscg <becegebcg@gmail.com>
2019-11-10 14:04:03 +0100

    Formulario de sign-up con Fetch + cambios de las variables para matcheo con backend

--- frontend-app/src/FormSignUp.js
+++ frontend-app/src/FormSignUp.js
@@ -6,7 +6,7 @@ class SignUpPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: '',
+            firstName: '',
             lastName:'',
             email:'',
             password: '',
@@ -14,7 +14,7 @@ class SignUpPage extends Component {
         };
 
         this.handlePassChange = this.handlePassChange.bind(this);
-        this.handleUserChange = this.handleUserChange.bind(this);
+        this.handleFirstChange = this.handleFirstChange.bind(this);
         this.handleLastChange = this.handleLastChange.bind(this);
         this.handleEmailChange = this.handleEmailChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -28,7 +28,7 @@ class SignUpPage extends Component {
     handleSubmit(evt) {
         evt.preventDefault();
 
-        if (!this.state.username) {
+        if (!this.state.firstName) {
             return this.setState({ error: 'First Name is required' });
         }
         if (!this.state.lastName) {
@@ -41,10 +41,10 @@ class SignUpPage extends Component {
         if (!this.state.password) {
             return this.setState({ error: 'Password is required' });
         }
-        console.log('State : ' + this.state);
-        console.log('User name : ' + this.state.username);
+        //console.log('State : ' + this.state);
+        //console.log('User name : ' + this.state.firstName);
 
-        console.log('User Email : ' + this.state.email);
+        //console.log('User Email : ' + this.state.email);
         const fetchData = async () => {
             const url = 'http://127.0.0.1:80/user';
             const options = {
@@ -77,9 +77,9 @@ class SignUpPage extends Component {
 
     }
 
-    handleUserChange(evt) {
+    handleFirstChange(evt) {
         this.setState({
-            username: evt.target.value,
+            firstName: evt.target.value,
         });
     };
 
@@ -116,17 +116,17 @@ render(){
                                 </h3>
                             }
                             <label>First Name</label>
-                            <input type="text" data-test="username" value={this.state.username} onChange={this.handleUserChange} />
+                            <input type="text"  value={this.state.firstName} onChange={this.handleFirstChange} />
                             <label>Last Name</label>
-                            <input type="text" data-test="lastName" value={this.state.lastName} onChange={this.handleLastChange} />
+                            <input type="text"  value={this.state.lastName} onChange={this.handleLastChange} />
                             <label>Email</label>
-                            <input type="email" data-test="email" value={this.state.email} onChange={this.handleEmailChange} />
+                            <input type="email"  value={this.state.email} onChange={this.handleEmailChange} />
 
                             <label>Password (6 or more characters)</label>
-                            <input type="password" data-test="password" value={this.state.password} onChange={this.handlePassChange} />
+                            <input type="password"  value={this.state.password} onChange={this.handlePassChange} />
                                 <p>You agree to the LinkedIn User Agreement, Privacy Policy, and Cookie Policy</p>
 
-                            <input type="submit" value="Agree & Join" data-test="submit" />
+                            <input type="submit" value="Agree & Join" />
                                 <p>Already on LinkedIn? Sign in</p>
                         </form>
                 </header>
