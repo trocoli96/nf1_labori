2477990bc3ae973cb5f31553a6a6d6bc779490f5
Francoeloy-dev <francoeloygonzalez@gmail.com>
2019-11-10 18:06:21 +0100

    Routing first try

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './App'
 import ButtonPopup from './Buttonpopup';
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
     return (
+        <Router>
         <div className="App">
             <header className="App-header">
-                <ButtonPopup open={"hola"}/>
+                <nav>
+                   Labori
+                </nav>
             </header>
+         <div className="body">
+             <Route path="/home"/>
+             <ButtonPopup open={"Editbutton"}/>
+         </div>
         </div>
+        <Router/>
     );
 }
 
-export default App;
+export default App;;
