b7253cdac2dd44270bca4beb8311f12b6dcd18cf
sofia <sofia.lanzinger@gmail.com>
2019-11-11 21:27:33 +0100

    Login & Login Form

--- frontend-app/src/index.js
+++ frontend-app/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-
-
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById('root'));
