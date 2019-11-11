600f501997b379b7f0ba6ae5e11e0c2ce658919b
sofia <sofia.lanzinger@gmail.com>
2019-11-11 20:33:50 +0100

    New branch DEV

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import './App.css';
+import LoginForm from "./Login-Form";
+import Logo from "./linkedin-logo.png";
+
 
 function App() {
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+        <div className="login">
+            <header className="login-header">
+                <img className={"logo-labori"} src={Logo} alt={"logo-linkedin"}/>
+                <h1 className={"login-welcome"}> Welcome Back</h1>
+                <p className={"login-text"}>Don't miss your next opportunity. Sign in to stay updated on your professional world.</p>
             </header>
+            <div className={"login-body"}>
+            <div className={"login-form-section"}>
+                <LoginForm/>
+            </div>
+            <div className={"login-form-links"}>
+                <div><a href="#"> Forgot Password?</a> </div>
+                <p>New to LaBori? <a href="#">Join now</a></p>
+            </div>
+        </div>
         </div>
     );
 }
