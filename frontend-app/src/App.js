fbf45b2e30d9e9d0e6ad713be6344389305290b8
sofia <sofia.lanzinger@gmail.com>
2019-11-11 21:46:06 +0100

    Login & Login Form

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Header from './Header';
 import Profilepage from "./Profilepage";
-import FormSingUp from './FormSignUp';
+import FormSignUp from './FormSignUp';
 import './App.css';
 import Header from "./Header";
 import Login from "./Login";
-import Router from "react-router-dom/es/Router";
-import Route from "react-router-dom/es/Route";
+
+
 
 const App = () => {
     return (
@@ -16,11 +15,11 @@ const App = () => {
             <Router>
                 <Route path={'/Home'}/>
                 <Route path={'/Login'} component={Login}/>
-                <Route path={'/Singup'} component={FormSingUp}/>
+                <Route path={'/Singup'} component={FormSignUp}/>
                 <Route path={'/Profile'} component={Profilepage}/>
             </Router>
         </div>
     );
-}
+};
 
 export default App;
