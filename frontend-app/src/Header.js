ae9932be3c76326dabb761a2200fa2ba2db6ed1e
Francoeloy-dev <francoeloygonzalez@gmail.com>
2019-11-11 17:49:36 +0100

    Nav bar material ui

--- frontend-app/src/Header.js
+++ frontend-app/src/Header.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import './App.css';
-import logo from "./logo-icon.png";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-function Header() {
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+function ButtonAppBar() {
+    const classes = useStyles();
   return (
-    <nav className="Header">
-      <div className="Header-content">
-        <img alt="logo-icon" src={logo} className="icon-image"/>
-      </div>
-        <div className="search-bar">
-            Search
-        </div>
-    </nav>
+      <AppBar position="static">
+          <Toolbar>
+              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                  <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                  News
+              </Typography>
+              <Button color="inherit">Login</Button>
+          </Toolbar>
+      </AppBar>
   );
 }
 
-export default Header;
+export default ButtonAppBar;
