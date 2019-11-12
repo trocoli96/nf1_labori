8fef5bfad9105a7abc095b4792757556f1b5e0f1
Alfonso <alfonso.trocoli@gmail.com>
2019-11-12 18:42:12 +0100

    Login backend funcionando! Let's fucking go!

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App'
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Header from './Header';
-import Profilepage from "./Profilepage";
 import Profilepage from './Profilepage';
 import FormSingUp from './FormSignUp'
 
