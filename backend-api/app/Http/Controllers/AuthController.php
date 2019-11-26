4bc86d614fc1c5200b2887671db4c7d0c0576f5e
Alfonso <alfonso.trocoli@gmail.com>
2019-11-26 14:33:32 +0100

    [L2] Improve Login Back end with token validation
    Primeros intentos de poner el token, hay fallos

--- backend-api/app/Http/Controllers/AuthController.php
+++ backend-api/app/Http/Controllers/AuthController.php
@@ -63,8 +63,7 @@ class AuthController extends Controller
            return $errors;
        }
       elseif (Hash::check($input['password'],$passGetter)) {
-           return redirect()->action(
-               'AuthController@returnUser', ['id' => $idGetter]);
+           return $this ->respondWithToken(????);
            }
             else {
                return $errorpass;
@@ -116,6 +115,13 @@ class AuthController extends Controller
             }
         }
     }
-
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
 
 }
