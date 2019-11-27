10fb44f42c9ce9c65def1f93511002e5f4e86c4e
braiscg <becegebcg@gmail.com>
2019-11-27 19:22:08 +0100

    Cambio del signup a hooks
    [L2] Style Sign Up

--- frontend-app/src/FormSignUp.js
+++ frontend-app/src/FormSignUp.js
@@ -1,23 +1,56 @@
 import React, {useState} from 'react';
 import './App.css';
-import TextField from '@material-ui/core/TextField';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+
+const useStyles = makeStyles(theme => ({
+    '@global': {
+        body: {
+            backgroundColor: theme.palette.common.white,
+        },
+    },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
 
 const SignUpForm = () => {
-    const [first_name, setName]= useState('f');
-    const [last_name, setLastName]= useState('s');
-    const [email, setEmail] = useState('@');
+    const [first_name, setName]= useState('');
+    const [last_name, setLastName]= useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
 
     const data = {
-
         first_name: first_name,
-
         last_name: last_name,
-
         email: email,
-
         password: password,
     };
 
@@ -38,10 +71,12 @@ const SignUpForm = () => {
             return fetch(url, options)
                 .then(response => {
                     if (response.status === 201) {
-                        alert(response.statusText);
-                        return response.json();
+                        return response.text();
                     }
                     return Promise.reject(response.status);
+                }).then(response => {
+                    console.log(response);
+                    alert(response);
                 }).catch(error => {
                     setError(error);
                     alert(error);
@@ -49,74 +84,99 @@ const SignUpForm = () => {
         };
         fetchdata()
     };
+    const classes = useStyles();
     return (
-        <div className={"login-form_full"}>
+             <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <div className={classes.paper}>
+                        <Avatar className={classes.avatar}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Make the most of your professional life
+                        </Typography>
+                        <form className={classes.form} noValidate >
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        autoComplete="fname"
+                                        name="firstName"
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        id="firstName"
+                                        label="First Name"
+                                        autoFocus
+                                        type="text" value={first_name}
+                                        onChange={event => setName(event.target.value)}
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        id="lastName"
+                                        label="Last Name"
+                                        name="lastName"
+                                        autoComplete="lname"
+                                        type="text" value={last_name}
+                                        onChange={event => setLastName(event.target.value)}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        autoComplete="email"
+                                        type="email"
+                                        value={email}
+                                        onChange={event => setEmail(event.target.value)}/>
 
-            <form className="login-form" onSubmit={handleSubmit}>
-                <div className="field_login">
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="first_name"
-                        label="Nombre"
-                        name="first_name"
-                        autoComplete="Nombre"
-                        autoFocus
-                        type="text" value={first_name}
-                        onChange={event => setName(event.target.value)}/>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="current-password"
+                                        value={password} onChange={event => setPassword(event.target.value)} />
 
-
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="last_name"
-                        label="Apellido"
-                        name="last_name"
-                        autoComplete="apellido"
-                        autoFocus
-
-                        type="text" value={last_name}
-                        onChange={event => setLastName(event.target.value)}/>
-
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-
-                        type="email" data-test="email" value={email}
-                        onChange={event => setEmail(event.target.value)}/>
-
-                </div>
-                <div className="field_login">
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="password"
-                        label="Password"
-                        name="password"
-                        autoComplete="password"
-                        autoFocus
-                        type="password" data-test="password" value={password} onChange={event => setPassword(event.target.value)} />
-
-                </div>
-                <Button variant="contained" color="primary" type="submit" onSubmit={event => setPassword(event.target.value)}>
-                    Sign in
-                </Button>
-            </form>
-
-        </div>
-    );
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <FormControlLabel
+                                        control={<Checkbox value="allowExtraEmails" color="primary" />}
+                                        label="I accept terms and conditions"
+                                    />
+                                </Grid>
+                            </Grid>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                            >
+                                Sign Up
+                            </Button>
+                            <Grid container justify="flex-end">
+                                <Grid item>
+                                    <Link href="/login" variant="body2">
+                                        Already have an account? Sign in
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </form>
+                    </div>
+                </Container>
+);
 };
 export default SignUpForm ;
