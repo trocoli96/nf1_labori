999b3ae72a9fffac786c814ea21b3cc2195f0c77
Angel <angelbt91@gmail.com>
2019-12-10 19:14:19 +0100

    Pueh eso

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,28 +1,62 @@
-import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import React, {useReducer} from 'react';
+import {BrowserRouter as Router, Route, withRouter} from 'react-router-dom';
 import Profilepage from "./Profilepage";
 import FormSignUp from './FormSignUp';
 import './App.css';
 import Header from "./Header";
 import Login from "./Login";
 import {HOME, PROFILE, SIGNUP, LOGIN} from "./routes/routes";
-import {getToken} from "./utils/localStorage";
-import {withRouter} from 'react-router-dom';
+import {AuthProvider} from "./utils/AuthFront/context";
 
-const token = getToken();
+const DO_LOGIN = 'DO_LOGIN';
+const IS_FETCHING = 'IS_FETCHING';
+const GET_ERROR = 'GET_ERROR';
+const GET_TOKEN = 'GET_TOKEN';
+
+const initialState = {
+    token: null,
+    isFetching: false
+};
+
+const AuthReducer = (state = initialState, action) => {
+    const newState = {...state};
+    const {type} = {...action};
+
+    if (type === DO_LOGIN) {
+        // TODO Lógica de Login
+    }
+    if (type === GET_TOKEN) {
+        return initialState.token;
+    }
+    if (type === GET_ERROR) {
+        newState.token = null;
+        return 'ERROR MOTHERFUCKER';
+    }
+
+    if (type === IS_FETCHING) {
+        newState.isFetching = true;
+    }
+
+    return newState;
+};
+
+const [state, dispatch] = useReducer(AuthReducer, initialState);
 
 const App = () => {
-    if (!token) {
-        return (<Login/>)
-    }
+
+    // TODO: Guardar el token en una variable, para reutilizarlo en toda la app
+    // TODO: If (token) => actualizar initialState
+    // TODO: LOGOUT --> Borra localstorage, actualiza initialState al estado original
 
     return (<div className={'body'}>
             <Router>
-                <Header/>
-                <Route path={HOME} component={Profilepage}/>
-                <Route path={LOGIN} component={Login}/>
-                <Route path={SIGNUP} component={FormSignUp}/>
-                <Route path={PROFILE} component={Profilepage}/>
+                <AuthProvider value={{state, dispatch}}>
+                    <Header/>
+                    <Route path={HOME} component={Profilepage}/>
+                    <Route path={LOGIN} component={Login}/>
+                    <Route path={SIGNUP} component={FormSignUp}/>
+                    <Route path={PROFILE} component={Profilepage}/>
+                </AuthProvider>
             </Router>
         </div>
     );
--- frontend-app/src/Profilepage.js
+++ frontend-app/src/Profilepage.js
@@ -1,23 +1,23 @@
 import './App.css';
 import ButtonPopup from "./Buttonpopup";
-import React, {useEffect, useReducer} from 'react';
-import {getToken} from "./utils/localStorage";
-
+import React, {useEffect, useReducer, useContext} from 'react';
+import AuthContext from "./utils/AuthFront/context";
 
 function Profilepage() {
+
+    const AuthConsumer = useContext(AuthContext);
 
     const VIEW_USER = 'VIEW_USER';
     const SET_ERROR = 'SET_ERROR';
 
-    const token = getToken();
     const initialState = {
         userData: [],
         error: false,
     };
 
     const userReducer = (state = initialState, action) => {
-        const newState = { ...state };
-        const { type } = { ...action };
+        const newState = {...state};
+        const {type} = {...action};
 
         if (type === VIEW_USER) {
             newState.userData = action.userData;
@@ -33,48 +33,52 @@ function Profilepage() {
 
     useEffect(() => {
         const fetchData = async () => {
-            const url = `http://127.0.0.1/api/users/2?token=${token.access_token}`;
+            const url = `http://127.0.0.1/api/users/`;
+            const token = (event) => {
+                AuthConsumer.dispatch({type: 'GET_TOKEN'});
+            };
             const options = {
                 method: 'GET',
                 headers: new Headers({
                     Accept: 'application/json',
                     'Access-Control-Allow-Headers': 'Authorization',
                     'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + token.access_token,
                 }),
-                mode: 'cors',
+                mode: 'cors'
             };
-    return fetch(url, options)
-        .then(response => {
-            if (response.status === 200) {
-                return response.json();
-            }
-            return Promise.reject(response.status);
-        })
-        .then(data => {
-            dispatch({ type: VIEW_USER, userData: data});
-        })
-        .catch(error => dispatch({ type: SET_ERROR, error: true }));
-};
+
+            return fetch(url, options)
+                .then(response => {
+                    if (response.status === 200) {
+                        return response.json();
+                    }
+                    return Promise.reject(response.status);
+                })
+                .then(data => {
+                    dispatch({type: VIEW_USER, userData: data});
+                })
+                .catch(error => dispatch({type: SET_ERROR, error: true}));
+        };
         fetchData();
-
     }, []);
-            return state.userData ? (
-                <div className="profilepage">
 
-                    <div id="profile-info">
-                        <div className="user-info">
-                            <p>Name and Last Name: {state.userData.email}</p>
-                            <p>Former name</p>
-                            <p>City,Country</p>
-                        </div>
-                        <div className="user-edit">
-                            <ButtonPopup/>
-                        </div>
-                    </div>
+    return state.userData ? (
+        <div className="profilepage">
+            <div id="profile-info">
+                <div className="user-info">
+                    <p>Name and Last Name: {state.userData.email}</p>
+                    <p>Former name</p>
+                    <p>City,Country</p>
                 </div>
+                <div className="user-edit">
+                    <ButtonPopup/>
+                </div>
+            </div>
+        </div>
 
 
-            ) : <p>hola</p>;
-        }
+    ) : <p>hola</p>;
+}
 
-        export default Profilepage;
+export default Profilepage;
