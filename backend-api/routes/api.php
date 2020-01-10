a015aff8d9ed5a25339e5085e4d8b774807418d0
Angel <angelbt91@gmail.com>
2020-01-10 20:07:46 +0100

    Aplicada la revisión de backend de Eudald, a excepcion de una pendiente

--- backend-api/routes/api.php
+++ backend-api/routes/api.php
@@ -16,9 +16,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('users/', 'AuthController@me');
-Route::get('users/', 'AuthController@me');
 Route::post('/user', 'AuthController@createUser');
-Route::put('users/{id}', 'AuthController@me');
 Route::post('/edituser', 'AuthController@editUser');
 Route::post('/login', 'AuthController@login');
 
